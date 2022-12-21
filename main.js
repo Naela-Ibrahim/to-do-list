f9852f04fea7648549c461a646dc14f44e3feb15
Naela-Ibrahim <nanyachocolate@gmail.com>
2022-12-21 15:13:51 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+const button = document.querySelector("button")
+const input = document.querySelector("input")
+const form = document.querySelector("form")
+const container = document.getElementById("container")
+
+
+
+
+// container.addEventListener("click", (eo) => {
+//     if (eo.target.className=="icon-trash icon") {
+//       eo.target.parentElement.parentElement.remove()
+//     } else if(eo.target.className == "icon-angry2 icon") {
+//         eo.target.classList.add("dn")
+//         const heart = ` <span class="icon-heart"></span>`
+//         eo.target.parentElement.parentElement.getElementsByClassName("task-text")[0].classList.add("finish")
+//         eo.target.parentElement.innerHTML += heart    
+//     }
+//     else if(eo.target.className == "icon-heart") {
+//         eo.target.parentElement.parentElement.getElementsByClassName("task-text")[0].classList.remove("finish")
+//         eo.target.classList.add("dn")
+//         const angry = ` <span class="icon-angry2 icon"></span>`
+//         eo.target.parentElement.innerHTML += angry
+           
+//     }
+//     else if(eo.target.className == "icon-star icon") {
+       
+//         eo.target.classList.add("star")
+//         container.prepend(eo.target.parentElement)
+       
+           
+//     }
+//     else if(eo.target.className == "icon-star icon star") {
+       
+//         eo.target.classList.remove("star")
+//         container.append(eo.target.parentElement)
+       
+           
+//     }
+
+
+// })
+
+// by switch
+container.addEventListener("click", (eo) => {
+  switch (eo.target.className) {
+    case "icon-trash icon" :
+      eo.target.parentElement.parentElement.remove()
+      break
+   case  "icon-angry2 icon" :
+        eo.target.classList.add("dn")
+        const heart = ` <span class="icon-heart"></span>`
+        eo.target.parentElement.parentElement.getElementsByClassName("task-text")[0].classList.add("finish")
+        eo.target.parentElement.innerHTML += heart    
+    break
+    case "icon-heart" :
+        eo.target.parentElement.parentElement.getElementsByClassName("task-text")[0].classList.remove("finish")
+        eo.target.classList.add("dn")
+        const angry = ` <span class="icon-angry2 icon"></span>`
+        eo.target.parentElement.innerHTML += angry
+           
+    break
+    case "icon-star icon" :
+       
+        eo.target.classList.add("star")
+        container.prepend(eo.target.parentElement)
+       
+           
+    break
+  case  "icon-star icon star" : 
+       
+        eo.target.classList.remove("star")
+        container.append(eo.target.parentElement)
+       
+       break
+  }})    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener("submit", (eo) => {
+    eo.preventDefault()
+    const task = `<div class="task">
+    <span class="icon-star icon"></span>
+   <p class="task-text">${input.value}</p> 
+   
+   <div>
+    <span class="icon-trash icon"></span>
+   <span class="icon-angry2 icon"></span>
+   </div>
+   </div>`
+   container.innerHTML += task
+   input.value=""
+    
+})
